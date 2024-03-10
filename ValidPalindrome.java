0b739a9d96d13825a6dfba209acc8cc060e81396
Kevin Threadgill <k_threadgill@yahoo.com>
2024-03-10 17:59:27 -0400

    first commit

---
+++ ValidPalindrome.java
@@ -0,0 +1,20 @@
+
+public class ValidPalindrome {
+
+//	Write a function that takes a string, s, as an input and determines whether or not it is a palindrome.
+
+	  public static boolean isPalindrome(String s) {
+		    int left = 0;
+		    int right = s.length() - 1;
+		    while (left < right) {
+		      if (s.charAt(left) != s.charAt(right))
+		      {
+		        return false;
+		      }
+		      left = left + 1;
+		      right = right - 1;
+		    }
+		    return true;
+		  }
+	
+}
